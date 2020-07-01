cbd300f99152c4a0c36e408cb6e1c3148277cced
Ilias <gamidov@tut.by>
2020-07-01 19:39:56 +0300

    Mask for number phone & check language text input

---
+++ src/js/modules/checkTextInput.js
@@ -0,0 +1,13 @@
+const checkTextInput = (selector) => {
+
+    const txtInput = document.querySelectorAll(selector);
+    txtInput.forEach(input => {
+        input.addEventListener('keypress', function(e) {
+            if (e.key.match(/[^а-яё 0-9]/ig)) {
+                e.preventDefault();
+            }
+        });
+    });
+};
+
+export default checkTextInput;
